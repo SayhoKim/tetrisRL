38ca604a5b033a549b14414c8d3108847a95ab89
SehoKim <rlatpgh@gmail.com>
2019-10-29 01:14:29 +0900

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from lib.function import Trainer, Tester
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', type=str, default='configs/config.yaml', help='Configuration File Path')
-    parser.add_argument('--demo', action='store_true', default=False, help='DEMO Mode')
+    parser.add_argument('--demo', action='store_true', default=False, help='Demo Mode')
     opt = parser.parse_args()
 
     cfg_file = open(opt.cfg, 'r')
